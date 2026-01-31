28d095d6db3d54cbee4dbbe72819b952ffb27446
enarroied <eric.narro.ied@gmail.com>
2026-01-31 22:32:27 +0100

    separate algorithms from page 2/2 and fix arguments

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -1,2 +1,8 @@
+from .select_data_all_regions import (
+    get_df_map_color as get_df_map_color,
+)
+from .select_data_all_regions import (
+    get_df_wine_year_and_area as get_df_wine_year_and_area,
+)
 from .select_data_by_regions import clean_df_region_color as clean_df_region_color
 from .select_data_by_regions import create_df_region as create_df_region
--- src/pages/all_regions.py
+++ src/pages/all_regions.py
@@ -1,7 +1,6 @@
 from typing import Any
 
-import pandas as pd
-
+from algorithms import get_df_map_color, get_df_wine_year_and_area
 from config.config import df_wine_production, df_wine_with_geometry
 
 selected_year = "average"
@@ -24,88 +23,11 @@ area_type_list = ["AOC", "Region"]
 selected_area = area_type_list[0]
 
 
-def get_df_map_color(
-    year: str, color: str, df_wine_with_geometry: pd.DataFrame = df_wine_with_geometry
-) -> pd.DataFrame:
-    """Create a DataFrame for map coloring based on wine production data.
-
-    This function takes a specific year and wine color, and extracts relevant information
-    from the original wine production DataFrame (`df_wine_with_geometry`). It creates a DataFrame
-    suitable for map coloring, including information about regions, latitude, longitude, production,
-    size, and text (a field to display the hover in the map).
-
-    Args:
-        year (str): The selected year for wine production data.
-        color (str): The selected wine color (e.g., 'RED AND ROSE', 'WHITE').
-        df_wine_with_geometry (pd.DataFrame, optional): DataFrame containing wine production and
-        geographical information. Defaults to the global variable `df_wine_with_geometry`.
-
-    Returns:
-        df_map_color (pd.DataFrame): A DataFrame with information for map coloring.
-    """
-
-    df_geometry_color = df_wine_with_geometry[
-        df_wine_with_geometry["wine_type"] == color
-    ].copy()
-
-    df_map_color = df_geometry_color[["Region", "latitude", "longitude"]]
-
-    # Production is divided by 10 to show million Liters
-    df_map_color["Production"] = df_geometry_color[year] / 10
-
-    # this is only to display and acceptable size of the dots on the map, dividing by 5 is arbitrary:
-    df_map_color["size"] = df_map_color["Production"] / 5
-    df_map_color["text"] = (
-        df_map_color["Region"] + ": " + df_map_color["Production"].astype(str) + " Ml"
-    )
-
-    print(df_map_color.head())
-    return df_map_color
-
-
-def get_df_wine_year_and_area(
-    year: str, area_type: str, df_wine_production: pd.DataFrame = df_wine_production
-) -> pd.DataFrame:
-    """Create a DataFrame for wine production based on a specific year and area type.
-
-    This function takes a specific year and area type, and extracts relevant information
-    from the original wine production DataFrame (`df_wine_production`). It creates a DataFrame
-    suitable for displaying wine production data for a specified year and area type.
-
-    Args:
-        year (str): The selected year for wine production data.
-        area_type (str): The selected area type (e.g., 'AOC', 'Region').
-        df_wine_production (pd.DataFrame, optional): DataFrame containing wine production data.
-        Defaults to the global variable `df_wine_production`.
-
-    Returns:
-        df_wine_year (pd.DataFrame): A DataFrame with information for displaying wine production data.
-    """
-
-    df_wine_year = df_wine_production[[area_type, "wine_type"]].copy()
-
-    # Production is divided by 10 to show million Liters
-    df_wine_year["Production"] = df_wine_production[year] / 10
-    df_wine_year = df_wine_year.reset_index(drop=True)
-    df_wine_year = df_wine_year.rename(columns={area_type: "Region"})
-
-    df_wine_year = df_wine_year.groupby(["Region", "wine_type"]).sum()
-    df_wine_year = df_wine_year.reset_index()
-    df_wine_year = df_wine_year.sort_values(by=["Production"])
-
-    # Add a column to display a cleaner and shorter label in the bar chart
-    df_wine_year["Wine Region"] = (
-        (df_wine_year["Region"].str.replace(r"[a-zA-Z]/ ", "", regex=True))
-        .str.replace(r" \(.+", "", regex=True)
-        .str.replace(r" including.+", "", regex=True)
-    )
-
-    return df_wine_year
-
-
-df_wine_year = get_df_wine_year_and_area(selected_year, selected_area)
-df_map_red = get_df_map_color(selected_year, "RED AND ROSE")
-df_map_white = get_df_map_color(selected_year, "WHITE")
+df_wine_year = get_df_wine_year_and_area(
+    selected_year, selected_area, df_wine_production
+)
+df_map_red = get_df_map_color(selected_year, "RED AND ROSE", df_wine_with_geometry)
+df_map_white = get_df_map_color(selected_year, "WHITE", df_wine_with_geometry)
 
 # Variables for the labels:
 total_production = df_wine_year["Production"].sum()
@@ -134,7 +56,7 @@ def on_change(state: Any) -> None:
     print("Chosen year: ", state.selected_year)
     print("Choose region type: ", state.selected_area)
     state.df_wine_year = get_df_wine_year_and_area(
-        state.selected_year, state.selected_area
+        state.selected_year, state.selected_area, df_wine_production
     )
     # Update the labels:
     state.total_production = state.df_wine_year["Production"].sum()
@@ -146,8 +68,12 @@ def on_change(state: Any) -> None:
     ].sum()
 
     # Update map dataframes:
-    state.df_map_red = get_df_map_color(state.selected_year, "RED AND ROSE")
-    state.df_map_white = get_df_map_color(state.selected_year, "WHITE")
+    state.df_map_red = get_df_map_color(
+        state.selected_year, "RED AND ROSE", df_wine_with_geometry
+    )
+    state.df_map_white = get_df_map_color(
+        state.selected_year, "WHITE", df_wine_with_geometry
+    )
 
 
 ##############################################################################################################
